22334166154b90c404b4d139da0a64e834d7b98b
Bernardo Rocha Salgueiro <Bernardo@Bernardo-PC.Home>
2015-07-14 23:04:38 -0300

    Change GameActivity and GameView
    Signed-off-by: Bernardo Rocha Salgueiro <Bernardo@Bernardo-PC.Home>

--- src/com/book/pong_v1/GameActivity.java
+++ src/com/book/pong_v1/GameActivity.java
@@ -5,11 +5,16 @@ import com.book.simplegameengine_v1.SGPreferences;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
-public class GameActivity extends SGActivity {
+public class GameActivity extends SGActivity 
+		implements GestureDetector.OnGestureListener{
 	
 	public static final String TAG = "PongV1";
 	private GameView mView;
+	
+	private GestureDetector mGestureDetector;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,8 @@ public class GameActivity extends SGActivity {
 		enableKeepScreenOn();
 		
 		SGPreferences preferences = getPreferences();
+		
+		mGestureDetector = new GestureDetector(this, this);
 		
 		if(preferences.getInt("first_time", -1) == -1){
 			preferences.begin()
@@ -40,9 +47,48 @@ public class GameActivity extends SGActivity {
 		stringBuilder.append("High score: ");
 		stringBuilder.append(preferences.getInt("high_score", 0));
 		Log.d(TAG, stringBuilder.toString());
-		
-		
-		
+				
 	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		return mGestureDetector.onTouchEvent(event);
+	}
+	
+	@Override
+	public boolean onDown(MotionEvent event) {
+		Log.d(TAG, "onDown() chamado."); 
+		return true;
+	}
+	
+	@Override
+	public boolean onFling(MotionEvent downEvent, MotionEvent moveEvent2, float velocityX, float velocityY) {
+		Log.d(TAG,  "onFling() chamado.");
+		return true;
+	}
+	
+	@Override
+	public void onLongPress(MotionEvent evemt) {
+		Log.d(TAG, "OnLongPress() chamado."); 
+	}
+	
+	@Override
+	public boolean onScroll(MotionEvent downEvent, MotionEvent moveEvent, float distanceX,
+			float distanceY) {
+		mView.movePlayer(-distanceX,  -distanceY);
+		Log.d(TAG,  "onScroll() chmado.");
+		return true;
+	}
+	
+	@Override
+	public void onShowPress(MotionEvent event) {
+		Log.d(TAG,  "onShowPress() chamado.");
+	}
+	
+	public boolean onSingleTapUp(MotionEvent event) {
+		Log.d(TAG,  "onSingleTapUp() chamado.");
+		return true;
+	}
+	
 	
 }
